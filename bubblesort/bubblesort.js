ecef6f7750c9d24f37702618bcad642b8f3b364f
Duncan <duncangichimu@gmail.com>
2018-11-05 11:32:20 -0800

    implement bubble sort

--- bubblesort/bubblesort.js
+++ bubblesort/bubblesort.js
@@ -1,11 +1,25 @@
 function bubbleSort(array) {
-
+  let sorted = false;
+  let j = 0;
+  while (!sorted) {
+    let isSorted = true;
+    for (let i = 0; i < array.length - j - 1; i++) {
+      if (compare(array[i], array[i + 1])) {
+        swap.call(array, i, i + 1);
+        isSorted = false;
+      }
+    }
+    j += 1;
+    sorted = isSorted;
+  }
 }
 
-swap = function() {
+swap = function(i, j) {
+  let temp = this[i];
+  this[i] = this[j];
+  this[j] = temp;
+};
 
-}
-
-compare = function() {
-
-}
+compare = function(i, j) {
+  return i > j;
+};
